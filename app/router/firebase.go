6ea11d7db242f1fb5dde4b26a13b77a0214972e4
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2024-05-26 10:40:17 +0000

    シークレットの設定を追加

--- app/router/firebase.go
+++ app/router/firebase.go
@@ -3,14 +3,21 @@ package router
 import (
 	"context"
 	"log"
+	"os"
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
+	"google.golang.org/api/option"
 )
 
 func NewFirebaseAuth() *auth.Client {
 	log.Printf("Start Firebase Setup")
-	app, err := firebase.NewApp(context.Background(), nil)
+	creds := os.Getenv("SECRET_PATH")
+	if creds == "" {
+		log.Fatalf("'SECRET_PATH' is not found.")
+	}
+	opt := option.WithCredentialsFile(creds)
+	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		log.Fatalf("error getting Auth client: %v\n", err)
 	}
