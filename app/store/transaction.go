326a175783bd219db99b652e16db61f206f86e52
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2024-07-21 02:52:48 +0000

    支払い方法毎の取引取得にオーダーの追加

--- app/store/transaction.go
+++ app/store/transaction.go
@@ -296,6 +296,7 @@ func (ts *TransactionStore) GetGroupByPayment(userId int, month string) *[]model
 		Where("t.transaction_date BETWEEN ? AND LAST_DAY(?)", month, month).
 		Order("payment_amount").
 		Order("t.transaction_date DESC").
+		Order("t.transaction_id DESC").
 		Scan(&payment_group_transaction)
 
 	return &payment_group_transaction
